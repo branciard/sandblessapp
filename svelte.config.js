95fe519200df2e1b8fc3e91b829f3afdd3f064e0
fbranciard <francoisbranciard@gmail.com>
2023-11-30 16:17:34 +0100

    modify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from "@sveltejs/adapter-static";
-//import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 
@@ -16,9 +15,6 @@ const config = {
 		}
 	}),
 	
-	vitePlugin: {
-		inspector: true,
-	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
