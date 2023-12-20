731c04406387030df62e5e7f9bcfee6f2f347688
fbranciard <francoisbranciard@gmail.com>
2023-12-20 16:26:07 +0100

    remove pyhton call test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [
+		sveltekit(),
+		purgeCss()
 });
